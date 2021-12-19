6423017a28b3b998c26af8832b2ed53779b59e42
KunzxD404 <91834999+KunzxD404@users.noreply.github.com>
2021-12-19 14:55:19 +0700

    Add files via upload

--- miku.js
+++ miku.js
@@ -21,11 +21,11 @@ const { getBuffer, getGroupAdmins, getRandom } = require('./lib/myfunc')
 
 //●●●●●●●●●●●●●●●●●●●●●● SETTING ●●●●●●●●●●●●●●●●●●●●●●                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               //BASE ORI BY KUNZXD🐦        
 
-botName = 'Miku Chan'
-ownerName = 'KxD'
+botName = 'MIKU BOT'
+ownerName = 'KxD' 
 ownerNumber = '6287778886786'
 faketeks = '𝙺𝚄𝙽𝚉𝙱𝙾𝚃𝚉︎'
-mypp = fs.readFileSync('thumbnail.jpg')
+mypp = fs.readFileSync('thumbnail.jpg') // thumbnailnya
 
 //●●●●●●●●●●●●●●●●●●●●●● DATABASE ●●●●●●●●●●●●●●●●●●●●●●
 
@@ -34,7 +34,7 @@ let welkom = JSON.parse(fs.readFileSync('./database/welcome.json'))
 
 //●●●●●●●●●●●●●●●●●●●●●● END JSON ●●●●●●●●●●●●●●●●●●●●●●
 
-module.exports = kunz = async (kunz, kxd) => {
+module.exports = miku = async (miku, kxd) => {
 try {
 if (!kxd.hasNewMessage) return
 kxd = kxd.messages.all()[0]
@@ -81,12 +81,13 @@ const args = body.trim().split(/ +/).slice(1)
 const arg = body.substring(body.indexOf(' ') + 1)
 const isCmd = body.startsWith(prefix) 
 const q = args.join(' ')
-const botNumber = kunz.user.jid
+const markon = "0@s.whatsapp.net"
+const botNumber = miku.user.jid
 const isGroup = from.endsWith('@g.us')
 const sender = isGroup ? kxd.participant : kxd.key.remoteJid
-const senderr = kxd.key.fromMe ? kunz.user.jid : kxd.key.remoteJid.endsWith('@g.us') ? kxd.participant : kxd.key.remoteJid
-const totalchat = await kunz.chats.all()
-const groupMetadata = isGroup ? await kunz.groupMetadata(from) : ''
+const senderr = kxd.key.fromMe ? miku.user.jid : kxd.key.remoteJid.endsWith('@g.us') ? kxd.participant : kxd.key.remoteJid
+const totalchat = await miku.chats.all()
+const groupMetadata = isGroup ? await miku.groupMetadata(from) : ''
 const groupName = isGroup ? groupMetadata.subject : ''
 const groupId = isGroup ? groupMetadata.jid : ''
 const groupMembers = isGroup ? groupMetadata.participants : ''
@@ -95,8 +96,8 @@ const groupOwner = isGroup ? groupMetadata.owner : ''
 const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
 const isBotGroupAdmins = groupAdmins.includes(botNumber) || false
 const isGroupAdmins = groupAdmins.includes(sender) || false
-const conts = kxd.key.fromMe ? kunz.user.jid : kunz.contacts[sender] || { notify: jid.replace(/@.+/, '') }
-const pushname = kxd.key.fromMe ? kunz.user.name : conts.notify || conts.vname || conts.name || '-'
+const conts = kxd.key.fromMe ? miku.user.jid : miku.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+const pushname = kxd.key.fromMe ? miku.user.name : conts.notify || conts.vname || conts.name || '-'
 const mentionByTag = type == "extendedTextMessage" && kxd.message.extendedTextMessage.contextInfo != null ? kxd.message.extendedTextMessage.contextInfo.mentionedJid : []
 const mentionByreply = type == "extendedTextMessage" && kxd.message.extendedTextMessage.contextInfo != null ? kxd.message.extendedTextMessage.contextInfo.participant || "" : ""
 const mention = typeof(mentionByTag) == 'string' ? [mentionByTag] : mentionByTag
@@ -122,17 +123,17 @@ admin: '*Khusus Leluhur Grup:v*'
 selectedButton = (type == 'buttonsResponseMessage') ? kxd.message.buttonsResponseMessage.selectedButtonId : ''
 responseButton = (type == 'listResponseMessage') ? kxd.message.listResponseMessage.title : ''
 const sendMess = (hehe, teks) => {
-kunz.sendMessage(hehe, teks, text)
+miku.sendMessage(hehe, teks, text)
 }
 const mentions = (teks, memberr, id) => {
-(id == null || id == undefined || id == false) ? kunz.sendMessage(from, {text: teks.trim(), jpegThumbnail: fs.readFileSync('thumbnail.jpg')}, extendedText, { sendEphemeral: true, contextInfo: { "mentionedJid": memberr } }) : kunz.sendMessage(from, {text: teks.trim(), jpegThumbnail: fs.readFileSync('thumbnail.jpg')}, extendedText, { sendEphemeral: true, quoted: fstatus, contextInfo: { "mentionedJid": memberr } })
+(id == null || id == undefined || id == false) ? miku.sendMessage(from, {text: teks.trim(), jpegThumbnail: fs.readFileSync('thumbnail.jpg')}, extendedText, { sendEphemeral: true, contextInfo: { "mentionedJid": memberr } }) : miku.sendMessage(from, {text: teks.trim(), jpegThumbnail: fs.readFileSync('thumbnail.jpg')}, extendedText, { sendEphemeral: true, quoted: fstatus, contextInfo: { "mentionedJid": memberr } })
 }
 const reply = (teks) => { 
-kunz.sendMessage(from, teks, text, {quoted:kxd, thumbnail: mypp})
+miku.sendMessage(from, teks, text, {quoted:kxd, thumbnail: mypp})
 }
 const kick = function(from, orangnya){
 for (let i of orangnya){
-kunz.groupRemove(from, [i])
+miku.groupRemove(from, [i])
 }
 }
 const sendMediaURL = async(to, url, text="", mids=[]) =>{
@@ -159,24 +160,27 @@ mime = Mimetype.gif
 if(mime.split("/")[0] === "audio"){
 mime = Mimetype.mp4Audio
 }
-kunz.sendMessage(to, media, type, {quoted: fstatus, mimetype: mime, caption: text, thumbnail: Buffer.alloc(0), contextInfo: {"mentionedJid": mids}})
+miku.sendMessage(to, media, type, {quoted: fstatus, mimetype: mime, caption: text, thumbnail: Buffer.alloc(0), contextInfo: {"mentionedJid": mids}})
 fs.unlinkSync(filename)
 });
 }
 const createSerial = (size) => {
 return crypto.randomBytes(size).toString('hex').slice(0, size)
 }
+const costum = (pesan, tipe, target, target2) => {
+miku.sendMessage(from, pesan, tipe, { quoted: { key: { fromMe: false, participant: `${target}`, ...(from ? { remoteJid: from } : {}) }, message: { conversation: `${target2}` } } })
+}
 
 //●●●●●●●●●●●●●●●●●●●●●● FAKE ●●●●●●●●●●●●●●●●●●●●●●
 
-const fakestatus = (teks) => { kunz.sendMessage(from, teks, text, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "Subscribe My YT KunzxD", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('thumbnail.jpg'), "scansSidecar": "1W0XhfaAcDwc7xh1R8lca6Qg/1bB4naFCSngM2LKO2NoP5RI7K+zLw==" } }} }) }
+const fakestatus = (teks) => { miku.sendMessage(from, teks, text, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "Subscribe My YT KunzxD", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('thumbnail.jpg'), "scansSidecar": "1W0XhfaAcDwc7xh1R8lca6Qg/1bB4naFCSngM2LKO2NoP5RI7K+zLw==" } }} }) }
 const fstatus = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg","caption": "Subscribe My YT KunzxD", 'jpegThumbnail': fs.readFileSync('thumbnail.jpg')}}}
 
 //●●●●●●●●●●●●●●●●●●●●●● BUTTON ●●●●●●●●●●●●●●●●●●●●●●
 
 const sendButLocation = async (id, text1, desc1, loc1, but = [], options = {}) => {
 hehe = loc1
-mhan = await zero.prepareMessage(from, hehe, location)
+mhan = await miku.prepareMessage(from, hehe, location)
 const buttonMessages = {
 locationMessage: mhan.message.locationMessage,
 contentText: text1,
@@ -184,7 +188,7 @@ footerText: desc1,
 buttons: but,
 headerType: "LOCATION"
 }
-zero.sendMessage(id, buttonMessages, MessageType.buttonsMessage, options)
+miku.sendMessage(id, buttonMessages, MessageType.buttonsMessage, options)
 }
 
 //●●●●●●●●●●●●●●●●●●●●●● MEDIA DETECT ●●●●●●●●●●●●●●●●●●●●●●
@@ -210,14 +214,14 @@ if (isGroup && isAntiLink && !isOwner && !isGroupAdmins && isBotGroupAdmins){
 if (budy.match(/(https:\/\/chat.whatsapp.com)/gi)) {
 fakestatus(`*「 GROUP LINK TERDETEKSI 」*\n\nSepertinya kamu mengirimkan link nomor, maaf kamu akan di kick:)`)
 setTimeout( () => {
-kunz.groupRemove(from, [sender])}, 1000)
+miku.groupRemove(from, [sender])}, 1000)
 }
 }
 if (isGroup && isAntiLink && !isOwner && !isGroupAdmins && isBotGroupAdmins){
 if (budy.match(/(https:\/\/wa.me)/gi)) {
 fakestatus(`*「 NOMOR LINK TERDETEKSI 」*\n\nSepertinya kamu mengirimkan link nomor, maaf kamu akan di kick:)`)
 setTimeout( () => {
-kunz.groupRemove(from, [sender])}, 1000)
+miku.groupRemove(from, [sender])}, 1000)
 }
 }
 
@@ -231,10 +235,15 @@ console.log(color('[ CMD ]', 'red'), color(time), color(`${command} [${args.leng
 //●●●●●●●●●●●●●●●●●●●●●● CASE/COMMAND ●●●●●●●●●●●●●●●●●●●●●●
 
 switch (command) {
+case 'command':
 case 'menu':
-teks = `Hi Kak ${pushname} Saya ${botName}
+const menunya = `Hi Kak ${pushname} Saya ${botName}
 
 ┏ *List Menu Bot*
+┃
+┣${prefix}leave
+┃
+┣${prefix}linkgroup
 ┃
 ┣${prefix}antilink
 ┃
@@ -252,11 +261,26 @@ teks = `Hi Kak ${pushname} Saya ${botName}
 ┃
 ┣${prefix}hidetag
 ┃
+┣${prefix}sticker
+┃
+┣${prefix}attp
+┃
 ┣${prefix}owner
 ┃
 ┗ *more?tambahin sendiri*
-`
-sendButlocation(from, teks, `${time}`, {jpegThumbnail:mypp,name:""}, [{buttonId:`.sc`,buttonText:{displayText:'Script'},type:1}], {contextInfo: { mentionedJid: [sender]}})
+
+Script : https://github.com/KunzxD404/mikuBase`
+miku.sendMessage(from, mypp, image, { quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "mimetype": "image/jpeg",  "caption": faketeks,  "jpegThumbnail": fs.readFileSync('thumbnail.jpg') } } }, caption: menunya })
+break
+case 'leave': 
+if (!isGroup) return reply(mess.only.group)
+if (!isGroupAdmins) return reply(mess.only.admin)
+setTimeout( () => {
+miku.groupLeave(from) 
+}, 2000)
+setTimeout( () => {
+reply('Byee...')
+}, 0)
 break
 case 'antilink':
 if (!isGroup) return reply(mess.only.group)
@@ -294,6 +318,16 @@ reply('Sukses menonaktifkan fitur welcome di group ini ✔️')
 reply('1 untuk mengaktifkan, 0 untuk menonaktifkan')
 }
 break
+case 'link': 
+case 'linkgc': 
+case 'linkgrup' :
+case 'linkgroup': 
+if (!isGroup) return reply(mess.only.group)
+if (!isBotGroupAdmins) return reply(mess.only.Badmin)
+linkgc = await miku.groupInviteCode(from)
+yeh = `https://chat.whatsapp.com/${linkgc}`
+miku.sendMessage(from, yeh, text, { quoted: fstatus })
+break
 case 'promote' :
 if (!isGroup) return reply(mess.only.group)
 if (!isGroupAdmins) return reply(mess.only.admin)
@@ -306,10 +340,10 @@ for (let _ of mentioned) {
 teks += `@${_.split('@')[0]}\n`
 }
 mentions(teks, mentioned, true)
-kunz.groupMakeAdmin(from, mentioned)
+miku.groupMakeAdmin(from, mentioned)
 } else {
 mentions(`Selamat @${mentioned[0].split('@')[0]} Kamu Telah Manjadi Leluhur Di Grup Ini`, mentioned, true)
-kunz.groupMakeAdmin(from, mentioned)
+miku.groupMakeAdmin(from, mentioned)
 }
 break
 case 'demote' :
@@ -324,10 +358,10 @@ for (let _ of mentioned) {
 teks += `@${_.split('@')[0]}\n`
 }
 mentions(teks, mentioned, true)
-kunz.groupDemoteAdmin(from, mentioned)
+miku.groupDemoteAdmin(from, mentioned)
 } else {
 mentions(`Maaf Ya @${mentioned[0].split('@')[0]} Kamu kuturunkan menjadi member:)`, mentioned, true)
-kunz.groupDemoteAdmin(from, mentioned)
+miku.groupDemoteAdmin(from, mentioned)
 }
 break
 case 'add':
@@ -338,7 +372,7 @@ if (args.length < 1) return reply('Nomer Yg Mau Di Add Mana ?')
 if (args[0].startsWith('08')) return reply('Gunakan Kode Negara Gan')
 try {
 num = `${args[0].replace(/ /g, '')}@s.whatsapp.net`
-kunz.groupAdd(from, [num])
+miku.groupAdd(from, [num])
 } catch (e) {
 console.log('Error :', e)
 reply('Gagal menambahkan target')
@@ -351,7 +385,7 @@ if (!isBotGroupAdmins) return reply(mess.only.Badmin)
 if(!q)return reply(`*Format Error!*\n\n*Example : ${prefix + command} @tag*`)
 if (!isBotGroupAdmins) return reply(mess.only.Badmin)
 y = q.split('@')[1] + '@s.whatsapp.net'
-kunz.groupRemove(from, [y])
+miku.groupRemove(from, [y])
 reply(`Succes kick target😼`)
 break
 case 'opengc' :
@@ -359,14 +393,14 @@ if (!isGroup) return reply(mess.only.group)
 if (!isGroupAdmins) return reply(mess.only.admin)
 if (!isBotGroupAdmins) return reply(mess.only.Badmin)
 reply(`✓Sukses Membuka Group *${groupMetadata.subject}*`)
-kunz.groupSettingChange(from, GroupSettingChange.messageSend, false)
+miku.groupSettingChange(from, GroupSettingChange.messageSend, false)
 break
 case 'closegc' :
 if (!isGroup) return reply(mess.only.group)
 if (!isGroupAdmins) return reply(mess.only.admin)
 if (!isBotGroupAdmins) return reply(mess.only.Badmin)
 reply(`✓Sukses Menutup Group *${groupMetadata.subject}*`)
-kunz.groupSettingChange(from, GroupSettingChange.messageSend, true)
+miku.groupSettingChange(from, GroupSettingChange.messageSend, true)
 break
 case 'hidetag':
 if (!isGroup) return reply(mess.only.group)
@@ -382,7 +416,7 @@ break
 case 'owner':
 let inilist = []
 for (let i of ownerNumber) {
-let vname = kunz.contacts[i] != undefined ? kunz.contacts[i].vname || kunz.contacts[i].notify : undefined
+let vname = miku.contacts[i] != undefined ? miku.contacts[i].vname || miku.contacts[i].notify : undefined
 inilist.push({
 "displayName": `${ownerName}`,
 "vcard": 'BEGIN:VCARD\n'
@@ -393,10 +427,89 @@ inilist.push({
 + 'END:VCARD'.trim()
 })
 }
-kD = await kunz.sendMessage(from, {
+kD = await miku.sendMessage(from, {
 "displayName": `${inilist.length} kontak`,
 "contacts": inilist 
 }, 'contactsArrayMessage', { quoted: kxd })
+break
+case 'sticker':  case 'stiker':  case 's': 
+if ((isMedia && !kxd.message.videoMessage || isQuotedImage) && args.length == 0) {
+const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(kxd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : kxd
+const media = await miku.downloadAndSaveMediaMessage(encmedia, 'media_user')
+ran = getRandom('.webp')
+await ffmpeg(`./${media}`)
+.input(media)
+.on('start', function (cmd) {
+console.log(`Started : ${cmd}`)
+})
+.on('error', function (err) {
+console.log(`Error : ${err}`)
+fs.unlinkSync(media)
+reply(mess.error.stick)
+})
+.on('end', function () {
+console.log('Finish')
+buffer = fs.readFileSync(ran)
+costum(buffer, sticker, markon, `Jangan Lupa SUBSCRIBE YT : KUNZXD`)
+fs.unlinkSync(media)
+fs.unlinkSync(ran)
+})
+.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+.toFormat('webp')
+.save(ran)
+} else if ((isMedia && kxd.message.videoMessage.seconds < 11 || isQuotedVideo && kxd.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(kxd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : kxd
+const media = await miku.downloadAndSaveMediaMessage(encmedia, 'media_user')
+ran = getRandom('.webp')
+reply(mess.wait)
+await ffmpeg(`./${media}`)
+.inputFormat(media.split('.')[1])
+.on('start', function (cmd) {
+console.log(`Started : ${cmd}`)
+})
+.on('error', function (err) {
+console.log(`Error : ${err}`)
+fs.unlinkSync(media)
+tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+reply(`❌ Gagal, pada saat mengkonversi ${tipe} ke stiker. pastikan untuk video yang dikirim tidak lebih dari 9 detik`)
+})
+.on('end', function () {
+console.log('Finish')
+costum(fs.readFileSync(ran), sticker, markon, `~ Nih Dah Jadi Gif Stikernya`)
+fs.unlinkSync(media)
+fs.unlinkSync(ran)
+})
+.addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+.toFormat('webp')
+.save(ran)
+} else if ((isMedia || isQuotedImage) && args[0] == 'nobg') {
+const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(kxd).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : kxd
+const media = await miku.downloadAndSaveMediaMessage(encmedia, 'media_user')
+ranw = getRandom('.webp')
+ranp = getRandom('.png')
+reply(mess.wait)
+keyrmbg = 'bcAvZyjYAjKkp1cmK8ZgQvWH'
+await removeBackgroundFromImageFile({ path: media, apiKey: keyrmbg, size: 'auto', type: 'auto', ranp }).then(res => {
+fs.unlinkSync(media)
+let buffer = Buffer.from(res.base64img, 'base64')
+fs.writeFileSync(ranp, buffer, (err) => {
+if (err) return reply('Gagal, Terjadi kesalahan, silahkan coba beberapa saat lagi.')
+})
+exec(`ffmpeg -i ${ranp} -vcodec libwebp -filter:v fps=fps=20 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${ranw}`, (err) => {
+fs.unlinkSync(ranp)
+if (err) return reply(mess.error.stick)
+miku.sendMessage(from, fs.readFileSync(ranw), sticker, { quoted: kyy })
+fs.unlinkSync(ranw)
+})
+})
+} else {
+reply(`Kirim gambar dengan caption ${prefix}sticker atau tag gambar yang sudah dikirim`)
+}
+break
+case 'attp': 
+if (args.length == 0) return reply(`Example: ${prefix + command} Hai`)
+buffer = await getBuffer(`https://api.xteam.xyz/attp?file&text=${q}`)
+miku.sendMessage(from, buffer, sticker, { quoted: fstatus })
 break
 case 'sc': case 'sourcecode': case 'script':
 reply('https://github.com/KunzxD404/MikuBase\n\nPlease Take Star⭐\n\nAnd follow:)')
